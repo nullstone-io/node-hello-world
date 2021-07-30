43b81d1b46b2b83d1b0851e9276d1443a5f46a87
Scott Sickles <scottsickles@gmail.com>
2021-07-30 14:31:05 -0400

    started adding the prisma client

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const express = require('express'),
-    morgan = require('morgan');
+const { PrismaClient } = require('@prisma/client')
+const express = require('express'), morgan = require('morgan');
 const app = express();
-
 const port = process.env.PORT || 3000,
     env = process.env.NULLSTONE_ENV || 'local';
+const prisma = new PrismaClient();
+
+async function getTodos() {
+    const todos = await prisma.todo.findMany()
+    console.log(todos)
+}
 
 app.use(morgan('combined'))
 app.use(express.static('public'));
 app.get('/', function (req, res) {
     res.redirect('/index.html');
+})
+
+app.get('/todos', async (req, res) => {
+    try {
+        const todos = await prisma.todo.findMany()
+        res.json(todos)
+    } finally {
+        await prisma.$disconnect()
+    }
 })
 
 const server = app.listen(port, () => {
